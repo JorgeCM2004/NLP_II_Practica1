1504c952e6bb3c0f831090a1f2d528a0f026d7a7
albertopeerez <albertoperezparedes@gmail.com>
2025-11-07 18:10:17 +0100

    lots of fn

--- src/genre_classification/F_Basic_Models.py
+++ src/genre_classification/F_Basic_Models.py
@@ -1,11 +1,68 @@
 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+
+from .F_Compute_Metrics import Compute_Metrics
+from .F_Preprocess_Data import Preprocess_Data
 
 from typing import Literal
 
-class Basic_Models:
-    
-    def __init__(self, model_type: Literal['Random_Forest', 'Naive_Bayes', ' LogReg', 'Linear_SVM'] ):
-        pass
+class Basic_Models(Preprocess_Data, Compute_Metrics):
 
+    def __init__(self, model_type: Literal['Random_Forest', 'Naive_Bayes', 'LogReg', 'Linear_SVM']):
+        
+        self.model_type = model_type
+        self.model = self._select_model()
+        self.prep = Preprocess_Data()
+        self.objectPrepro = None
 
+        
+    def _select_model(self):
+        if self.model_type == 'Random_Forest':
+            return RandomForestClassifier(random_state=42)
+        elif self.model_type == 'Naive_Bayes':
+            return GaussianNB()
+        elif self.model_type == 'LogReg':
+            return LogisticRegression(solver='liblinear', random_state=42)
+        elif self.model_type == 'Linear_SVM':
+            return SVC(kernel='linear', probability=True, random_state=42)
+        else:
+            raise ValueError(f"Tipo de modelo desconocido: {self.model_type}")
+        
+    def fit(self, data):
+        x_train, y_train = self.split_data(data)
+        x_train['text'] = self.prep.preprocessing_data(x_train['text'])
+        texts_transformed,_,self.objectPrepro = self.prep.data_representation(x_train['text'], 'tf-idf')
+        x_train['text'] = texts_transformed
+        self.model.fit(x_train, y_train)
+         
 
+    def transform(self, data):
+
+        x_test, y_test = self.split_data(data) 
+        
+        x_test['text'] = self.prep.preprocessing_data(x_test['text'])
+
+        x_test['texts'] = self.objectPrepro.transform(x_test['text'])
+        
+        y_hat = self.model.predict(x_test)
+        
+        return y_hat, y_test
+        
+        
+    def evaluate(y_true, y_hat, evaluate_type : Literal["sk_learn_metrics", "all_metrics"]):
+        Compute_Metrics(y_hat, y_true)
+        
+        if evaluate_type.lower() == "all_metrics":
+            return Compute_Metrics.compute_all()
+        
+        elif evaluate_type.lower() == "sk_learn_metrics":
+            return Compute_Metrics.full_report()
+        
+        else:
+            raise ValueError(f"Not metric type found: {evaluate_type}")
+        
+        
+        
--- src/genre_classification/F_Preprocess_Data.py
+++ src/genre_classification/F_Preprocess_Data.py
@@ -1,19 +1,25 @@
-import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import spacy
-import re
-import warnings
-from spacy.util import WN_EXTRA_FEATURES
 from typing import Literal
-
+from sklearn.model_selection import train_test_split
+from .config import SEED
 
 '''
 añadir n-gramas y limite del vocabulario
 
 '''
 
-class Preprocess_Data():
+class Preprocess_Data:
     
+    def __init__(self):
+        try:
+            self.nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+            print("Modelo de spaCy 'en_core_web_sm' cargado.")
+        except IOError:
+            print("Error: El modelo 'en_core_web_sm' de spaCy no está instalado.")
+            print("Ejecuta: python -m spacy download en_core_web_sm")
+            self.nlp = None
+            
     def data_representation(self, data, weight_type: Literal['tf-idf', 'binary', 'frequency'] = 'tf-idf'):
 
         weight_type = weight_type.lower()
@@ -42,20 +48,6 @@ class Preprocess_Data():
         
         
 
-
-warnings.filterwarnings("ignore", category=UserWarning, message=f"^{re.escape(WN_EXTRA_FEATURES)}")
-
-class TextPreprocessor:
-    
-    def __init__(self):
-        try:
-            self.nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
-            print("Modelo de spaCy 'en_core_web_sm' cargado.")
-        except IOError:
-            print("Error: El modelo 'en_core_web_sm' de spaCy no está instalado.")
-            print("Ejecuta: python -m spacy download en_core_web_sm")
-            self.nlp = None
-
     def preprocessing_data(self, data: list[str]) -> list[list[str]]:
 
         if self.nlp is None:
@@ -78,3 +70,8 @@ class TextPreprocessor:
         text =  [" ".join(doc_tokens) for doc_tokens in data_preprocesed]
             
         return text
+    
+    def split_data(self, data, test_size=0.2):
+    
+        train_set, test_set = train_test_split(data, test_size=test_size, random_state=SEED, shuffle=True, stratify= data['genre'])
+        return train_set, test_set
